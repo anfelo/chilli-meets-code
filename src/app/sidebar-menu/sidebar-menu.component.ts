d26ba9bdee6a9a80043ce43dad80753e6af4906c
anfelo <af.osorio1341@gmail.com>
2018-08-11 18:52:11 -0500

    feature: Add responsive breakpoints

--- src/app/sidebar-menu/sidebar-menu.component.ts
+++ src/app/sidebar-menu/sidebar-menu.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
   styleUrls: ['./sidebar-menu.component.scss']
 })
-export class SidebarMenuComponent {}
+export class SidebarMenuComponent {
+  public isMenuCollapsed: boolean = false;
+
+  constructor() {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize($event) {
+    const width = $event.target.innerWidth;
+    if (width > 425 && this.isMenuCollapsed) {
+      this.toggleMenu($event);
+    }
+  }
+
+  public toggleMenu($event) {
+    this.isMenuCollapsed = !this.isMenuCollapsed;
+  }
+}

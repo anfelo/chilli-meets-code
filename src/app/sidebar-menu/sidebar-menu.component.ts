e245272792af9ed932baafd41c9287633b266432
anfelo <af.osorio1341@gmail.com>
2018-08-11 19:32:18 -0500

    feature: Refactor sidebar menu items

--- src/app/sidebar-menu/sidebar-menu.component.ts
+++ src/app/sidebar-menu/sidebar-menu.component.ts
@@ -1,14 +1,46 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'sidebar-menu',
   templateUrl: './sidebar-menu.component.html',
   styleUrls: ['./sidebar-menu.component.scss']
 })
-export class SidebarMenuComponent {
+export class SidebarMenuComponent implements OnInit {
   public isMenuCollapsed: boolean = false;
 
+  public menuItems: any[];
+
   constructor() {}
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        text: 'WELCOME',
+        url: '/home',
+        isActive: true
+      },
+      {
+        text: 'ABOUT ME',
+        url: '/about',
+        isActive: false
+      },
+      {
+        text: 'BLOG',
+        url: '/blog',
+        isActive: false
+      },
+      {
+        text: 'COURSES',
+        url: '/course',
+        isActive: false
+      },
+      {
+        text: 'GET IN TOUCH',
+        url: '/contact',
+        isActive: false
+      }
+    ];
+  }
 
   @HostListener('window:resize', ['$event'])
   onResize($event) {
